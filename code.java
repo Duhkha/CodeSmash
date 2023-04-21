89f8fb5a39e51f9386d1f897487cb2b50714219e
Duhkha <116498714+Duhkha@users.noreply.github.com>
2023-04-21 15:11:46 +0200

    Update code.java

--- code.java
+++ code.java
@@ -1,8 +1,11 @@
-import java.util.List;
+package org.example;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
-public class Foo {
+class Foo {
     /*
      * Complete the 'IsBuyerWinner' function below.
      *
@@ -12,12 +15,62 @@ public class Foo {
      * 2. List (STRING_ARRAY) - shoppingCart
      */
     public static int IsBuyerWinner(List<String> codeList, List<String> shoppingCart) {
-        // TODO: Implement the function
-        return 0;
+        if (codeList == null || codeList.isEmpty()) {
+            // If codeList is empty, then the buyer is a winner
+            return 1;
+        }
+        if (shoppingCart == null || shoppingCart.isEmpty()) {
+            // If shoppingCart is empty, then the buyer is not a winner
+            return 0;
+        }
+        int i = 0, j = 0;
+        while (i < codeList.size() && j < shoppingCart.size()) {
+            String[] code = codeList.get(i).split(" ");
+            if (code.length == 1 && code[0].equals("anything")) {
+                // If code has only "anything", then any fruit can be in its place
+                i++;
+                continue;
+            }
+            int k = j;
+            while (k < shoppingCart.size()) {
+                String[] cart = shoppingCart.get(k).split(" ");
+                if (code[0].equals(cart[0]) || code[0].equals("anything")) {
+                    // If first fruit in code matches current fruit in shoppingCartor it's "anything"
+                    boolean found = true;
+                    for (int l = 1; l < code.length; l++) {
+                        if (k+l >= shoppingCart.size()) {
+                            // If code has more fruits than remaining shoppingCart items, then break
+                            found = false;
+                            break;
+                        }
+                        cart = shoppingCart.get(k+l).split(" ");
+                        if (!code[l].equals(cart[0]) && !code[l].equals("anything")) {
+                            // If any other fruit in code does not match with the next item in shoppingCart, then break
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        // If all fruits in code match with shoppingCart items, then move on to next group
+                        i++;
+                        j = k + code.length;
+                        break;
+                    }
+                }
+                k++;
+            }
+            if (k == shoppingCart.size()) {
+                // If no match found in shoppingCart for current code group, then buyer is not a winner
+                return 0;
+            }
+        }
+        return i == codeList.size() ? 1 : 0;
     }
-}
 
-public class Solution {
+
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int codeListCount = Integer.parseInt(scanner.nextLine().trim());
@@ -36,4 +89,7 @@ public class Solution {
         System.out.println(foo);
         scanner.close();
     }
+
+
 }
+
